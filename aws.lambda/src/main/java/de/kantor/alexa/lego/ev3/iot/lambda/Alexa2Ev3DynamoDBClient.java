8a66cbf53522092ffc01b863d8ae746486c115e0
alexa2lego <kontakt@kantor-online.de>
2017-08-09 15:00:23 +0200

    Update Alexa2Ev3DynamoDBClient.java

--- aws.lambda/src/main/java/de/kantor/alexa/lego/ev3/iot/lambda/Alexa2Ev3DynamoDBClient.java
+++ aws.lambda/src/main/java/de/kantor/alexa/lego/ev3/iot/lambda/Alexa2Ev3DynamoDBClient.java
@@ -26,76 +26,76 @@ import com.amazonaws.services.dynamodbv2.model.ScanResult;
  */
 public class Alexa2Ev3DynamoDBClient {
 
-	private static final String EV3_DEVICES_TABLE = "ev3device";
-	private static final Logger LOG = LoggerFactory.getLogger(Alexa2Ev3DynamoDBClient.class);
-	private static Alexa2Ev3DynamoDBClient instance;
+    private static final String EV3_DEVICES_TABLE = "ev3device";
 
-	private static AmazonDynamoDB amazonDynamoDbClient;
+    private static final Logger LOG = LoggerFactory.getLogger(Alexa2Ev3DynamoDBClient.class);
 
-	private Alexa2Ev3DynamoDBClient(String clientEndpoint, String clientId, String awsAccessKeyId,
-			String awsSecretAccessKey) {
-		BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKeyId, awsSecretAccessKey);
-		amazonDynamoDbClient = AmazonDynamoDBClientBuilder.standard()
-				.withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
-	}
+    private static Alexa2Ev3DynamoDBClient instance;
 
-	public static synchronized Alexa2Ev3DynamoDBClient getInstance() {
-		if (instance == null) {
-			instance = new Alexa2Ev3DynamoDBClient(System.getenv("aws_iot_endpoint"), System.getenv("aws_iot_client"),
-					System.getenv("aws_iot_accessKeyId"), System.getenv("aws_iot_secretAccessKey"));
-		}
-		return instance;
-	}
+    private static AmazonDynamoDB amazonDynamoDbClient;
 
-	public List<Ev3Device> findAllDevices() {
+    private Alexa2Ev3DynamoDBClient(String awsAccessKeyId, String awsSecretAccessKey) {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKeyId, awsSecretAccessKey);
+        amazonDynamoDbClient = AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
+    }
 
-		List<Ev3Device> ev3Devices = new ArrayList<>();
+    public static synchronized Alexa2Ev3DynamoDBClient getInstance() {
+        if (instance == null) {
+            instance = new Alexa2Ev3DynamoDBClient(System.getenv("aws_iot_accessKeyId"),
+                    System.getenv("aws_iot_secretAccessKey"));
+        }
+        return instance;
+    }
 
-		ScanResult result = null;
+    public List<Ev3Device> findAllDevices() {
 
-		do {
-			ScanRequest req = new ScanRequest();
-			req.setTableName(EV3_DEVICES_TABLE);
+        List<Ev3Device> ev3Devices = new ArrayList<>();
 
-			if (result != null) {
-				req.setExclusiveStartKey(result.getLastEvaluatedKey());
-			}
+        ScanResult result = null;
 
-			result = amazonDynamoDbClient.scan(req);
+        do {
+            ScanRequest req = new ScanRequest();
+            req.setTableName(EV3_DEVICES_TABLE);
 
-			List<Map<String, AttributeValue>> rows = result.getItems();
+            if (result != null) {
+                req.setExclusiveStartKey(result.getLastEvaluatedKey());
+            }
 
-			for (Map<String, AttributeValue> map : rows) {
-				try {
-					String iotName = map.get("thing").getS();
-					Map<String, AttributeValue> device = map.get("payload").getM().get("state").getM().get("reported")
-							.getM().get("device").getM();
+            result = amazonDynamoDbClient.scan(req);
 
-					List<AttributeValue> commands = device.get("commands").getL();
-					Map<String, String> commandsMap = new HashMap<>();
-					for (AttributeValue command : commands) {
-						Entry<String, AttributeValue> entry = command.getM().entrySet().iterator().next();
-						String actionword = entry.getKey();
-						String commandTexts = entry.getValue().getS();
-						List<String> commandArray = new ArrayList<>(Arrays.asList(commandTexts.split(",")));
-						commandArray.stream()
-								.forEach(c -> commandsMap.put(c.trim().toLowerCase(), actionword.toUpperCase()));
+            List<Map<String, AttributeValue>> rows = result.getItems();
 
-					}
-					Ev3Device ev3Device = new Ev3Device(device.get("serialNumber").getS(), iotName,
-							device.get("deviceName").getS().toLowerCase());
-					ev3Device.setCommands(commandsMap);
-					ev3Devices.add(ev3Device);
+            for (Map<String, AttributeValue> map : rows) {
+                try {
+                    String iotName = map.get("thing").getS();
+                    Map<String, AttributeValue> device = map.get("payload").getM().get("state").getM().get("reported")
+                            .getM().get("device").getM();
 
-				} catch (NumberFormatException e) {
-					LOG.error(e.getMessage());
-				}
-			}
-		} while (result.getLastEvaluatedKey() != null);
+                    List<AttributeValue> commands = device.get("commands").getL();
+                    Map<String, String> commandsMap = new HashMap<>();
+                    for (AttributeValue command : commands) {
+                        Entry<String, AttributeValue> entry = command.getM().entrySet().iterator().next();
+                        String actionword = entry.getKey();
+                        String commandTexts = entry.getValue().getS();
+                        List<String> commandArray = new ArrayList<>(Arrays.asList(commandTexts.split(",")));
+                        commandArray.stream()
+                                .forEach(c -> commandsMap.put(c.trim().toLowerCase(), actionword.toUpperCase()));
 
-		LOG.info("count of ev3Devices: " + ev3Devices.size());
+                    }
+                    Ev3Device ev3Device = new Ev3Device(device.get("serialNumber").getS(), iotName,
+                            device.get("deviceName").getS().toLowerCase());
+                    ev3Device.setCommands(commandsMap);
+                    ev3Devices.add(ev3Device);
 
-		return ev3Devices;
-	}
+                } catch (NumberFormatException e) {
+                    LOG.error(e.getMessage());
+                }
+            }
+        } while (result.getLastEvaluatedKey() != null);
 
+        LOG.info("count of ev3Devices: " + ev3Devices.size());
+
+        return ev3Devices;
+    }
 }
