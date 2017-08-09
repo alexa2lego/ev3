cecf30e540930d4b5793320043a431b12717c5dd
alexa2lego <kontakt@kantor-online.de>
2017-08-09 15:05:54 +0200

    Update Alexa2Ev3IotMessage.java

--- aws.lambda/src/main/java/de/kantor/alexa/lego/ev3/iot/lambda/Alexa2Ev3IotMessage.java
+++ aws.lambda/src/main/java/de/kantor/alexa/lego/ev3/iot/lambda/Alexa2Ev3IotMessage.java
@@ -5,35 +5,17 @@ import com.amazonaws.services.iot.client.AWSIotQos;
 
 public class Alexa2Ev3IotMessage extends AWSIotMessage {
 
-	public Alexa2Ev3IotMessage(String topic, AWSIotQos qos) {
-		super(topic, qos);
-	}
+    public Alexa2Ev3IotMessage(String topic, AWSIotQos qos) {
+        super(topic, qos);
+    }
 
-	public Alexa2Ev3IotMessage(String topic, AWSIotQos qos, byte[] payload) {
-		super(topic, qos, payload);
-	}
+    public Alexa2Ev3IotMessage(String topic, AWSIotQos qos, byte[] payload) {
+        super(topic, qos, payload);
+    }
 
-	public Alexa2Ev3IotMessage(String topic, AWSIotQos qos, String payload) {
-		super(topic, qos, payload);
+    public Alexa2Ev3IotMessage(String topic, AWSIotQos qos, String payload) {
+        super(topic, qos, payload);
 
-	}
-
-	@Override
-	public void onSuccess() {
-		// TODO Auto-generated method stub
-		super.onSuccess();
-	}
-
-	@Override
-	public void onFailure() {
-		// TODO Auto-generated method stub
-		super.onFailure();
-	}
-
-	@Override
-	public void onTimeout() {
-		// TODO Auto-generated method stub
-		super.onTimeout();
-	}
+    }
 
 }
