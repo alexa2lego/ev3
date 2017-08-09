99f4bde611f8874c062eceb94728a2dd4fb2a1d0
alexa2lego <kontakt@kantor-online.de>
2017-08-09 15:05:27 +0200

    Update Alexa2Ev3IotClient.java

--- aws.lambda/src/main/java/de/kantor/alexa/lego/ev3/iot/lambda/Alexa2Ev3IotClient.java
+++ aws.lambda/src/main/java/de/kantor/alexa/lego/ev3/iot/lambda/Alexa2Ev3IotClient.java
@@ -16,107 +16,79 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * {@link Alexa2Ev3IotClient} creates a IoT client that retrieves current state
- * of EV3 IoT
+ * {@link Alexa2Ev3IotClient} creates an IoT client that sends command to EV3 devices
  * 
  * @author kantor
  *
  */
 public class Alexa2Ev3IotClient {
 
-	private static final Logger LOG = LoggerFactory.getLogger(Alexa2Ev3IotClient.class);
-	private static final long PUBLISH_TIMEOUT = 3000;
-	private static Alexa2Ev3IotClient instance;
-	private Map<String, AWSIotDevice> iotDevices = new HashMap<>();
-	private static ObjectMapper objectMapper;
-	private Map<String, AWSIotMqttClient> iotClients = new HashMap<>();
-	private Map<String, String> deviceMap = new HashMap<>();
+    private static final Logger LOG = LoggerFactory.getLogger(Alexa2Ev3IotClient.class);
 
-	private Alexa2Ev3IotClient(String clientEndpoint, String clientId, String awsAccessKeyId, String awsSecretAccessKey,
-			List<Ev3Device> ev3Devices) {
-		for (Ev3Device ev3Device : ev3Devices) {
-			AWSIotMqttClient iotClient = new AWSIotMqttClient(clientEndpoint, clientId, awsAccessKeyId,
-					awsSecretAccessKey);
-			iotClient.setKeepAliveInterval(5);
+    private static final long PUBLISH_TIMEOUT = 3000;
 
-			AWSIotDevice iotDevice = new AWSIotDevice(ev3Device.getIotName());
-			iotDevices.put(ev3Device.getDeviceName(), iotDevice);
-			deviceMap.put(ev3Device.getDeviceName(), ev3Device.getIotName());
-			try {
-				iotClient.attach(iotDevice);
-				LOG.info("Device " + ev3Device.getIotName() + " was attached");
-				iotClients.put(ev3Device.getDeviceName(), iotClient);
-			} catch (AWSIotException e) {
-				LOG.error("Device " + ev3Device.getIotName() + " can not be attached", e);
-			}
-		}
-		objectMapper = new ObjectMapper();
-		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-	}
+    private static Alexa2Ev3IotClient instance;
 
-	public static synchronized Alexa2Ev3IotClient getInstance(List<Ev3Device> ev3Devices) {
-		if (instance == null) {
+    private Map<String, AWSIotDevice> iotDevices = new HashMap<>();
 
-			instance = new Alexa2Ev3IotClient(System.getenv("aws_iot_endpoint"), System.getenv("aws_iot_client"),
-					System.getenv("aws_iot_accessKeyId"), System.getenv("aws_iot_secretAccessKey"), ev3Devices);
-		}
-		return instance;
-	}
+    private static ObjectMapper objectMapper;
 
-	/**
-	 * Retrieves state from the EV3 thing shadow
-	 * 
-	 * @param receiver
-	 * @return object of {@link Ev3Device}
-	 * @throws Alexa2Ev3Exception
-	 */
-	public Ev3Device getThingState(String receiver) throws Alexa2Ev3Exception {
-		try {
-			AWSIotMqttClient iotClient = iotClients.get(receiver);
-			if (iotClient == null) {
+    private Map<String, AWSIotMqttClient> iotClients = new HashMap<>();
 
-				throw new Alexa2Ev3Exception("ioT-Client " + receiver + " can not be found");
-			}
-			if (iotClient.getConnectionStatus().equals(AWSIotConnectionStatus.DISCONNECTED)) {
-				iotClient.connect();
-			}
-			AWSIotDevice iotDevice = iotDevices.get(receiver);
-			if (iotDevice == null) {
-				throw new Alexa2Ev3Exception("Device " + receiver + " can not be found");
-			}
-			String shadowState = iotDevice.get();
+    private Map<String, String> deviceMap = new HashMap<>();
 
-			Ev3Device thingState = objectMapper.readValue(shadowState, Ev3Device.class);
-			// TODO
-			LOG.info(String.format("IoT Device has shadowState: %s", shadowState));
+    private Alexa2Ev3IotClient(String clientEndpoint, String clientId, String awsAccessKeyId,
+            String awsSecretAccessKey, List<Ev3Device> ev3Devices) {
+        for (Ev3Device ev3Device : ev3Devices) {
+            AWSIotMqttClient iotClient = new AWSIotMqttClient(clientEndpoint, clientId, awsAccessKeyId,
+                    awsSecretAccessKey);
+            iotClient.setKeepAliveInterval(5);
 
-			return thingState;
-		} catch (Exception e) {
-			throw new Alexa2Ev3Exception("sending request for current state failed", e);
-		}
-	}
+            AWSIotDevice iotDevice = new AWSIotDevice(ev3Device.getIotName());
+            iotDevices.put(ev3Device.getDeviceName(), iotDevice);
+            deviceMap.put(ev3Device.getDeviceName(), ev3Device.getIotName());
+            try {
+                iotClient.attach(iotDevice);
+                LOG.info("Device " + ev3Device.getIotName() + " was attached");
+                iotClients.put(ev3Device.getDeviceName(), iotClient);
+            } catch (AWSIotException e) {
+                LOG.error("Device " + ev3Device.getIotName() + " can not be attached", e);
+            }
+        }
+        objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
 
-	public void sendCommand(String deviceId, final Alexa2Ev3Command command) throws Alexa2Ev3Exception {
-		try {
-			AWSIotMqttClient iotClient = iotClients.get(deviceId);
-			if (iotClient == null) {
-				LOG.info("iotClients:" + iotClients);
-				throw new Alexa2Ev3Exception("ioT-Client " + deviceId + " can not be found");
-			}
+    public static synchronized Alexa2Ev3IotClient getInstance(List<Ev3Device> ev3Devices) {
+        if (instance == null) {
 
-			if (iotClient.getConnectionStatus().equals(AWSIotConnectionStatus.DISCONNECTED)) {
-				LOG.info("iotClient " + iotClients + " reconnected");
-				iotClient.connect();
-			}
+            instance = new Alexa2Ev3IotClient(System.getenv("aws_iot_endpoint"), System.getenv("aws_iot_client"),
+                    System.getenv("aws_iot_accessKeyId"), System.getenv("aws_iot_secretAccessKey"), ev3Devices);
+        }
+        return instance;
+    }
 
-			Alexa2Ev3IotMessage message = new Alexa2Ev3IotMessage(deviceMap.get(deviceId), AWSIotQos.QOS0,
-					command.toJson());
+    public void sendCommand(String deviceId, final Alexa2Ev3Command command) throws Alexa2Ev3Exception {
+        try {
+            AWSIotMqttClient iotClient = iotClients.get(deviceId);
+            if (iotClient == null) {
+                LOG.info("iotClients:" + iotClients);
+                throw new Alexa2Ev3Exception("ioT-Client " + deviceId + " can not be found");
+            }
 
-			iotClient.publish(message, PUBLISH_TIMEOUT);
-			LOG.info(String.format("Command %s sent to receiver %s", command.toJson(), deviceId));
-		} catch (Exception e) {
-			throw new Alexa2Ev3Exception("command can not be sent to receiver", e);
-		}
-	}
+            if (iotClient.getConnectionStatus().equals(AWSIotConnectionStatus.DISCONNECTED)) {
+                LOG.info("iotClient " + iotClients + " reconnected");
+                iotClient.connect();
+            }
+
+            Alexa2Ev3IotMessage message = new Alexa2Ev3IotMessage(deviceMap.get(deviceId), AWSIotQos.QOS0,
+                    command.toJson());
+
+            iotClient.publish(message, PUBLISH_TIMEOUT);
+            LOG.info(String.format("Command %s sent to receiver %s", command.toJson(), deviceId));
+        } catch (Exception e) {
+            throw new Alexa2Ev3Exception("command can not be sent to receiver", e);
+        }
+    }
 
 }
